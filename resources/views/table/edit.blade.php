42b4b198b5d34c761372ef5cad570bc576efa9ea
EDUARDOMP7 <eduardo639123@gmail.com>
2024-08-06 09:59:37 -0600

    Actualizacion de diseños

--- resources/views/table/edit.blade.php
+++ resources/views/table/edit.blade.php
@@ -5,23 +5,29 @@
 @endsection
 
 @section('content')
-    <section class="content container-fluid">
-        <div class="">
-            <div class="col-md-12">
+    <section class="container mx-auto p-6">
+        <div class="flex justify-center">
+            <div class="w-full max-w-2xl">
 
                 @includeif('partials.errors')
 
-                <div class="card card-default">
-                    <div class="card-header">
-                        <span class="card-title">{{ __('Update') }} Table</span>
+                <div class="bg-white shadow-md rounded-lg overflow-hidden">
+                    <div class="bg-gray-800 text-white py-3 px-6 rounded-t-lg">
+                        <h3 class="text-lg font-semibold">{{ __('Update') }} Table</h3>
                     </div>
-                    <div class="card-body">
-                        <form method="POST" action="{{ route('tables.update', $table->id) }}" role="form"
+                    <div class="p-6 bg-white">
+                        <form method="POST" action="{{ route('table.update', $table->id) }}" role="form"
                             enctype="multipart/form-data">
                             {{ method_field('PATCH') }}
                             @csrf
 
                             @include('table.form')
+
+                            <div class="mt-4">
+                                <button type="submit" class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700">
+                                    {{ __('Update') }}
+                                </button>
+                            </div>
 
                         </form>
                     </div>
